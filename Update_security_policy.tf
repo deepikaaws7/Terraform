88b60f5afc08d9d575504e23d23c9188df67c284
deepikaaws7 <71882019+deepikaaws7@users.noreply.github.com>
2020-09-25 17:37:30 +0100

    Create Update_security_policy.tf

---
+++ Update_security_policy.tf
@@ -0,0 +1,33 @@
+
+# Security groups are maintaing for Port 22 and Port 80
+# SSH port 22 details mentioned below
+
+resource "aws_security_group" "websg" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  # Http port security groups for Port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
